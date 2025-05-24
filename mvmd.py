2e2766238bb4e67acb287a111290d4ed02f89eff
WiwilZ <56433705+WiwilZ@users.noreply.github.com>
2025-05-24 16:40:57 +0800

    Support batched input

--- mvmd.py
+++ mvmd.py
@@ -8,7 +8,7 @@ def mvmd(signal: torch.Tensor, K: int, alpha=2000.0, tau=0.0, DC=False, init=1, 
     Multivariate Variational Mode Decomposition implemented in PyTorch.
 
     Args:
-        signal (torch.Tensor): Input tensor of shape (T, C) where T is signal length and C is channels.
+        signal (torch.Tensor): Input tensor of shape (B, T, C) or (T, C) where B is batch size, T is signal length and C is channels.
         K               (int): Number of modes to extract.
         alpha         (float): Bandwidth constraint parameter.
         tau           (float): Time-step for dual ascent (noise slack).
@@ -18,13 +18,19 @@ def mvmd(signal: torch.Tensor, K: int, alpha=2000.0, tau=0.0, DC=False, init=1, 
         N               (int): Maximum number of iterations.
 
     Returns:
-        u     (torch.Tensor): Reconstructed modes of shape (T, C, K).
-        u_hat (torch.Tensor): Spectra of modes, shape (T, C, K).
-        omega (torch.Tensor): Center frequencies per iteration, shape (n, K).
+        u     (torch.Tensor): Reconstructed modes of shape (B, T, C, K) or (T, C, K).
+        u_hat (torch.Tensor): Spectra of modes, shape (B, T, C, K) or (T, C, K).
+        omega (torch.Tensor): Center frequencies per iteration, shape (B, n, K) or (n, K).
     """
-    if signal.dim() != 2:
-        raise ValueError("signal must be a 2D tensor of shape (T, C)")
-    T, C = signal.size()
+    if signal.dim() == 2:
+        signal = signal.unsqueeze(0)
+        squeeze_output = True
+    elif signal.dim() == 3:
+        squeeze_output = False
+    else:
+        raise ValueError("signal must be a 2D tensor of shape (T, C) or 3D tensor of shape (B, T, C)")
+    
+    B, T, C = signal.size()
     dtype, device = signal.dtype, signal.device
     if dtype is torch.float32:
         ctype = torch.complex64
@@ -36,71 +42,76 @@ def mvmd(signal: torch.Tensor, K: int, alpha=2000.0, tau=0.0, DC=False, init=1, 
     """Preparations"""
     # Mirroring
     pad = T // 2
-    left = signal[:pad].flip(dims=(0,))           
-    right = signal[-pad:].flip(dims=(0,))        
-    f = torch.cat([left, signal, right], dim=0) 
-    L = f.size(0)  # 2T if T is even else 2T - 1 
+    left = signal[:, :pad].flip(dims=(1,))           
+    right = signal[:, -pad:].flip(dims=(1,))        
+    f = torch.cat([left, signal, right], dim=1)   # (B, L, C)
+    L = f.size(1)  # 2T if T is even else 2T - 1 
     freqs = torch.arange(L, device=device, dtype=dtype) / L - 0.5  # (L,)
     # Construct and center f_hat
-    f_hat_plus = torch.fft.fftshift(torch.fft.fft(f, dim=0), dim=0)  # (L, C)
-    f_hat_plus[:L // 2] = 0
+    f_hat_plus = torch.fft.fftshift(torch.fft.fft(f, dim=1), dim=1)  # (B, L, C)
+    f_hat_plus[:, :L // 2] = 0
 
     """Initialization"""
     # matrix keeping track of every iterant
-    u_hat_plus = torch.zeros(L, C, K, dtype=ctype, device=device)
-    u_hat_prev = u_hat_plus.clone()
+    u_hat_plus = torch.zeros(B, L, C, K, dtype=ctype, device=device)  # (B, L, C, K)
+    u_hat_prev = u_hat_plus.clone()  # (B, L, C, K)
     omega_plus = []
     # initialize omegas uniformly
     if init == 1:
-        omega_plus.append((0.5 / K) * torch.arange(K, dtype=dtype, device=device))
+        omega = ((0.5 / K) * torch.arange(K, dtype=dtype, device=device)).unsqueeze(0).expand(B, K).contiguous()
     elif init == 2:
-        omega_plus.append((math.log(0.5 * T) * torch.rand(K, dtype=dtype, device=device).sort()[0]).exp() / T)
+        omega = ((math.log(0.5 * T) * torch.rand(B, K, dtype=dtype, device=device)).exp() / T).sort(dim=-1)[0]
     else:
-        omega_plus.append(torch.zeros(K, dtype=dtype, device=device))
+        omega = torch.zeros(B, K, dtype=dtype, device=device)
     # if DC mode imposed, set its omega to 0
     if DC:
-        omega_plus[-1][0] = 0
+        omega[:, 0] = 0
+    omega_plus.append(omega)  # (B, K)
 
     """Algorithm of MVMD"""
-    u_diff = tol + torch.finfo(float).eps
-    n = 0
-    lambda_hat = torch.zeros(L, C, dtype=ctype, device=device)
-    sum_uk = torch.zeros(L, C, dtype=ctype, device=device)
-    while u_diff > tol and n < N - 1:
-        # update first mode
-        sum_uk += u_hat_prev[:, :, -1] - u_hat_prev[:, :, 0]
-        u_hat_plus[:, :, 0] = (f_hat_plus - sum_uk - lambda_hat / 2) / (1 + alpha * (freqs.unsqueeze(-1) - omega_plus[-1][0]).square())
-        # update other modes
-        for k in range(1, K):
-            sum_uk += u_hat_plus[:, :, k - 1] - u_hat_prev[:, :, k]
-            u_hat_plus[:, :, k] = (f_hat_plus - sum_uk - lambda_hat / 2) / (1 + alpha * (freqs.unsqueeze(-1) - omega_plus[-1][k]).square())
+    lambda_hat = torch.zeros(B, L, C, dtype=ctype, device=device)  # (B, L, C)
+    sum_uk = torch.zeros(B, L, C, dtype=ctype, device=device)  # (B, L, C)
+    for _ in range(N - 1):
+        # update modes
+        numer_tmp = f_hat_plus - 0.5 * lambda_hat  # (B, L, C)
+        denom = (1 + alpha * (freqs.view(1, -1, 1, 1) - omega_plus[-1].view(1, 1, 1, -1)).square()).reciprocal() # (1, L, 1, K)
+        for k in range(K):
+            # When k is 0, u_hat_plus[..., k - 1] is u_hat_prev[..., -1].
+            sum_uk += u_hat_plus[..., k - 1] - u_hat_prev[..., k]
+            u_hat_plus[..., k] = (numer_tmp - sum_uk) * denom[..., k]
         # center frequencies
-        power = u_hat_plus[L // 2:].abs().square()
-        omega_plus.append(torch.einsum('l,lck->k', freqs[L // 2:], power) / (power.sum(dim=(0, 1)) + torch.finfo(dtype).eps))
+        power = u_hat_plus[:, L // 2:].abs().square() # (B, L/2, C, K)
+        omega = torch.einsum('l,blck->bk', freqs[L // 2:], power) / (power.sum(dim=(1, 2)) + torch.finfo(dtype).eps)
         if DC:
-            omega_plus[-1][0] = 0
+            omega[:, 0] = 0
+        omega_plus.append(omega)
         # Dual ascent
         lambda_hat += tau * (u_hat_plus.sum(dim=-1) - f_hat_plus)
         # convergence
-        u_diff = (u_hat_plus - u_hat_prev).abs().square().sum().item() / L + torch.finfo(float).eps
+        u_diff = (u_hat_plus - u_hat_prev).abs().square().sum().item() / (B * L) + torch.finfo(float).eps
         u_hat_prev = u_hat_plus.clone()
-        n += 1
+        if u_diff <= tol:
+            break
 
     """Post-processing and cleanup"""
     # discard empty space if converged early
-    omega = torch.stack(omega_plus, dim=0)
+    omega = torch.stack(omega_plus, dim=1)
     # Signal reconstruction
     u_hat = torch.empty_like(u_hat_plus)
-    u_hat[L // 2:] = u_hat_plus[L // 2:]
-    u_hat[1:L // 2 + 1] = u_hat_plus[L // 2:].conj().flip(dims=(0,))
-    u_hat[0] = u_hat[-1].conj()
+    u_hat[:, L // 2:] = u_hat_plus[:, L // 2:]
+    u_hat[:, 1:L // 2 + 1] = u_hat_plus[:, L // 2:].conj().flip(dims=(1,))
+    u_hat[:, 0] = u_hat[:, -1].conj()
 
-    u = torch.fft.ifft(torch.fft.ifftshift(u_hat, dim=0), dim=0).real
+    u = torch.fft.ifft(torch.fft.ifftshift(u_hat, dim=1), dim=1).real
     # remove mirror part 
-    u = u[pad:-pad]
+    u = u[:, pad:-pad]  # (B, T, C, K)
     # recompute spectrum
-    u_hat = torch.fft.fftshift(torch.fft.fft(u, dim=0), dim=0)
+    u_hat = torch.fft.fftshift(torch.fft.fft(u, dim=1), dim=1)
 
+    # Squeeze output if input was non-batched
+    if squeeze_output:
+        u, u_hat, omega = u.squeeze(0), u_hat.squeeze(0), omega.squeeze(0)
+    
     return u, u_hat, omega
 
 
